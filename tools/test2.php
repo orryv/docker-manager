1b06eb1d2e23cdf89198c73a94fe6919c0f7819c
Orry <orryonline@hotmail.com>
2025-11-24 02:52:28 +0100

    test2

--- tools/test2.php
+++ tools/test2.php
@@ -18,7 +18,7 @@ if (!file_exists($debug)) {
     mkdir($debug, 0777, true);
 }
 
-$dcm = new DockerComposeManager(YamlParserFactory::getYamlExtensionParser());
+$dcm = DockerComposeManager::new();
 $dcm->debug($debug);
 $config = $dcm->fromDockerComposeFile('test', $composeFile);
 $dcm->startAsync(null, null, true);
@@ -28,4 +28,4 @@ do{
     // print_r($progress);
     echo 'Progress: ' . ($progress->getBuildLastLine() ?? '') . PHP_EOL;
     usleep(500000);
-} while (!$progress->isFinishedExecuting());
+} while (!$progress->isFinishedExecuting() && !$progress->isHealthy());
